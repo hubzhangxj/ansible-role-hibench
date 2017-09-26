f16a8bf678f905bab868780a4af0614a4f8ce716
James Zhang <zhangxinjia@huawei.com>
2017-09-26 21:58:31 +0800

    test with maven.

--- hibench_build.sh
+++ hibench_build.sh
@@ -2,6 +2,14 @@
 #CURDIR=$(cd `dirname $0`; pwd)
 pushd ../../ > /dev/null
 
-ansible-playbook -i hosts site.yml  --user=root --extra-vars "ansible_sudo_pass=root" &
+ansible-playbook -i hosts hibench.yml  --user=root --extra-vars "ansible_sudo_pass=root" &
 
 popd > /dev/null
+
+pushd ../../../../opt/hibench
+
+echo  "---------`pwd`---------"
+./bin/build-all.sh
+
+popd > /dev/null
+
